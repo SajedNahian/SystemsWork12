73072d5e53a33f79af936c44cf043f2e9dce6e9a
snahian <contactsajednahian@gmail.com>
2018-11-04 14:07:37 -0500

    Updated while loop, cleaned code

--- dirinfo.c
+++ dirinfo.c
@@ -6,34 +6,31 @@
 #include <string.h>
 #include <errno.h>
 
-int main(int argc, char *argv[]) {
+int main (int argc, char *argv[]) {
     char dir_path[500];
 
-    if(argc < 2){
-        printf("no command line argument found\n");
-
-        // keep prompting unil user enters a directory name
-        while(strlen(dir_path) == 0 || dir_path[0] == '\n'){
-            printf("enter a directory name: \n");
+    if (argc < 2) {
+        printf("No command line argument for directory found\n");
+        while (1) {
+            printf("Enter a directory name: ");
             fgets(dir_path, sizeof(dir_path), stdin);
+            if (!(strlen(dir_path) == 0 || dir_path[0] == '\n')) {
+                break;
+            }
         }
     } else {
         strcpy(dir_path, argv[1]);
     }
 
-    // printf("%s\n", dir_path);
-    printf("\n");
-
-    // remove newline from fgets
-    if(dir_path[strlen(dir_path) - 1] == '\n'){
+    if (dir_path[strlen(dir_path) - 1] == '\n'){
         dir_path[strlen(dir_path) - 1] = '\0';
     }
 
-    printf("looking at %s/:\n", dir_path);  
+    printf("Looking at directory '%s':\n", dir_path);  
 
     DIR * dir_stream = opendir(dir_path);
 
-    if(!dir_stream) {
+    if (!dir_stream) {
         printf("%s\n", strerror(errno));
         return -1; 
     }
@@ -48,10 +45,7 @@ int main(int argc, char *argv[]) {
         strcpy(filePath, "./");
         strcat(filePath, cur->d_name);
         stat(filePath, buffer);
-        //printf("%d", cur->d_type);
-        //printf("size of %s: %ld\n", cur->d_name, buffer->st_size);
         size += buffer -> st_size;
-
         printf("%s", cur->d_name);
         if (cur -> d_type == 4) {
             printf(" (Directory)");
@@ -61,7 +55,7 @@ int main(int argc, char *argv[]) {
         cur = readdir(dir_stream);
     }
 
-    printf("Size of files in directory (%s/): %d bytes\n", dir_path, size);
+    printf("Size of files in directory '%s': %d bytes\n", dir_path, size);
     closedir(dir_stream);
     return 0;
 }
